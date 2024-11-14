f08e856bd2d02691178a29266433036388c72278
Rithishreddy27 <rithishreddy719@gmail.com>
2024-11-14 15:30:46 +0530

    Added mavenproj files

---
+++ src/main/java/se_first_project/MavenProj/App.java
@@ -0,0 +1,13 @@
+package se_first_project.MavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Rithish Redddy 22bd1a0516" );
+    }
+}
